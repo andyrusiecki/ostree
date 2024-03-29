8ab21aa72adb2ce25294d7323e52567275d32c86
Andy Rusiecki <andy.rusiecki@gmail.com>
2024-03-29 13:38:51 -0400

    disabling hyprland builds, attempting silverblue font and post-install fixes

--- overlays/silverblue/tmp/install-fonts.sh
+++ overlays/silverblue/tmp/install-fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-google_fonts=()
+#google_fonts=()
 nerd_fonts=(
   FireCode
   FireMono
@@ -13,24 +13,23 @@ nerd_fonts=(
   UbuntuMono
 )
 
+# google_tmp=$(mktemp -d)
+# for font in ${google_fonts[@]}
+# do
+#   curl -L https://fonts.google.com/download?family=${font// /%20}.zip --output $google_tmp/$font.zip
+#   unzip $google_tmp/$font.zip -d /usr/share/fonts/google/$font/
+# done
 
-mkdir -p /usr/share/fonts/google
-mkdir -p /usr/share/fonts/nerd
-
-google_tmp=$(mktemp -d)
-for font in ${google_fonts[@]}
-do
-  curl -L https://fonts.google.com/download?family=${font// /%20}.zip --output $google_tmp/$font.zip
-  unzip $google_tmp/$font.zip -d /usr/share/fonts/google/$font/
-done
-
-rm -rf $google_tmp
+# rm -rf $google_tmp
 
 nerd_tmp=$(mktemp -d)
 for font in ${nerd_fonts[@]}
 do
   curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font.zip --output $nerd_tmp/$font.zip
-  unzip $nerd_tmp/$font.zip -d /usr/share/fonts/nerd/$font/
+  font_dir=$(echo $font | sed 's/\([A-Z]\)/-\L\1/g;s/^-//')
+  unzip $nerd_tmp/$font.zip -d /usr/share/fonts/nerd-$font/
 done
 
 rm -rf $nerd_tmp
+
+fc-cache -f /usr/share/fonts
